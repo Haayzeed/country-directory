fa68a08ee03787361785e3942d9644ce8b142efa
Dosunmu Afeez <dosunmuafeez37@gmail.com>
2020-08-10 14:53:10 +0100

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,59 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <form>
+      <select name="" id="" v-model="selectCountry" @change="changeState()">
+        <option value="">--Select Country--</option>
+          <option  v-for="(country, index) in countries" :key="country.id" :value="index">{{country.name}}</option>
+      </select> <br><br>
+      <select name="" id="" @change="changeCity()" v-model="selectState">
+          <option  v-for="(state, index) in states" :key="state.id" :value="index">{{state.name}}</option>
+      </select> <br><br>
+       <select name="" id="">
+          <option  v-for="(city, index) in cities" :key="city.id" :value="index">{{city.name}}</option>
+      </select>
+    </form>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return{
+      countries: {},
+      states: {},
+      selectCountry: '',
+      selectState: '',
+      cities: {}
+    }
+  },
+  methods: {
+    changeState(){
+      this.states = this.countries.find(
+      (tester) => 
+      tester.id === this.selectCountry
+      )
+      this.states = this.countries[this.selectCountry].states
+    },
+    changeCity(){
+      this.cities = this.countries.find(
+      (tester1) => 
+      tester1.id === this.selectState
+      )
+      this.cities = this.states[this.selectState].cities
+    }
+  },
+  created(){
+      this.$http.get("https://raw.githubusercontent.com/dr5hn/countries-states-cities-database/master/countries%2Bstates%2Bcities.json")
+        .then((response) => {
+          const resultArray = [];
+          for (let key in response.data){
+            response.data[key].id = key
+            resultArray.push(response.data[key]);
+            this.countries = resultArray;
+          }
+        })
+        .catch((error) => console.error(error));
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import Vue from 'vue'
+import Axios from 'axios'
 import App from './App.vue'
+
+Vue.prototype.$http = Axios
+// Vue.use(Axios)
+// const axios = require('Axios').default;
 
 Vue.config.productionTip = false
 
